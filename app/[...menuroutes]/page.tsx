20aabaf787ef17c4a4bdf60e0913dac41c210fcf
imkrishn <963gurudev@gmail.com>
2026-03-22 21:39:21 +0530

    feat: add profile card component UI and complete feature to view other user profiles insights

--- app/[...menuroutes]/page.tsx
+++ app/[...menuroutes]/page.tsx
@@ -1,10 +1,20 @@
 import Home from "@/components/Home";
+import ProfileCard from "@/components/Profile";
 
 const Page = async ({ params }: { params: any }) => {
   const paramValue = await params;
   const menuRoutes = paramValue.menuroutes;
 
-  return <div className="w-screen">{menuRoutes[0] === "home" && <Home />}</div>;
+  return (
+    <div className="w-screen">
+      {menuRoutes[0] === "home" && <Home />}
+      {menuRoutes[0] === "view" && (
+        <div className="min-h-screen  flex items-center justify-end lg:mx-14 ">
+          <ProfileCard />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Page;
